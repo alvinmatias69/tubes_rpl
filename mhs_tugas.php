705bd8e3dded6eec712ad93b2b79c435de2ae5f5
Matias Alvin <alvinmatias69@gmail.com>
2016-04-14 21:20:15 +0700

    penambahan fungsionalitas upload

--- mhs_tugas.php
+++ mhs_tugas.php
@@ -11,17 +11,28 @@
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$data = array(
 				'id_assignment' => $_POST['id_assignment'], 
-				'link_jawaban'	=> htmlspecialchars($_POST['link_jawaban']),
+				'link_jawaban'	=> "",
 				'id_dosen'		=> $_POST['id_dosen'],
 				'type'			=> 'tugas',
-				'tanggal'		=> date('d-m-y'),
+				'tanggal'		=> date('y-m-d'),
 				'id_mhs'		=> $_SESSION['id_mhs']
 			);
-			$sql = "insert into Assignment(id_assignment, type, tanggal, link_jawaban, id_mhs, id_dosen) values ('" . $data['id_assignment'] . "', '" . $data['type'] . "', '" . $data['tanggal'] . "', '" . $data['link_jawaban'] . "', '" . $data['id_mhs'] . "', '" . $data['id_dosen'] . "');";
-			$conn->exec($sql);
-			echo "Data berhasil diinput";
-			$sql = "insert into Kehadiran(tanggal, id_mhs) values ('" . $data['tanggal'] . "', '" . $date['id_mhs'] . "');";
-			$conn->exec($sql);
+			$target_file = "tugas/" . $data['id_assignment'] . "." . basename($_FILES["file_jawaban"]["type"]);
+			$type = pathinfo($target_file,PATHINFO_EXTENSION);
+			if ($type != "zip" && $type != "rar") {
+				echo "File yang diupload harus berekstensi .zip atau .rar";
+			} else{
+				$data['link_jawaban'] = $target_file;
+				if(move_uploaded_file($_FILES['file_jawaban']['tmp_name'], $target_file)){
+					$sql = "insert into Assignment(id_assignment, type, tanggal, link_jawaban, id_mhs, id_dosen) values ('" . $data['id_assignment'] . "', '" . $data['type'] . "', '" . $data['tanggal'] . "', '" . $data['link_jawaban'] . "', '" . $data['id_mhs'] . "', '" . $data['id_dosen'] . "');";
+					$conn->exec($sql);
+					echo "Data berhasil diinput";
+					$sql = "insert into Kehadiran(tanggal, id_mhs) values ('" . $data['tanggal'] . "', '" . $data['id_mhs'] . "');";
+					$conn->exec($sql);
+				} else{
+					echo "gagal upload";
+				}
+			}
 		}
 		$sql = "select tanggal, link_soal, id_dosen from Soal where substr(id_soal, 1, 2) = 'tg' and id_dosen = (select id_dosen_wali from Mahasiswa where id_mhs = '" . $_SESSION['id_mhs'] . "');";
 		$stmt = $conn->prepare($sql);
@@ -34,10 +45,10 @@
 				echo $result['tanggal'] . " ";
 				echo $result['link_soal'] . " ";
 	?>
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-		<input type="text" name="link_jawaban" placeholder="link jawaban" required>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+		<input type="file" name="file_jawaban" placeholder="link jawaban" required>
 		<input type="hidden" name="id_assignment" value="<?php echo 'tg' . $result['id_dosen'] . $result['tanggal']; ?>">
-		<input type="hidden" name="id_dosen" value="<?php echo $_result['id_dosen'];?>">
+		<input type="hidden" name="id_dosen" value="<?php echo $result['id_dosen'];?>">
 		<input type="submit" value="input"><br>
 	</form>
 	<?php
